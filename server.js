8b22e4bcf7a94596bdd9385518b5919a95289446
clarkc0de <Dzawacki@clarku.edu>
2025-11-18 17:47:22 -0500

    made skeleton
    made skeleton

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(3000, function () {
+    console.log("server started at 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/archive', function (req, res) {
+    res.sendFile(__dirname + "/public/archive.html");
+})
+
+app.get('/edit-figure', function (req, res) {
+    res.sendFile(__dirname + "/public/edit_figure.html");
+})
+
+app.get('/create_new', function (req, res) {
+    res.sendFile(__dirname + "/public/create_new.html");
+})
+
+app.get('/export_and_save', function (req, res) {
+    res.sendFile(__dirname + "/public/export_and_save.html");
+})
