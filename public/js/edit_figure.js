584de129005201527bcb8a0d3295ad4784f4a838
Robert Matzkin <119685237+RobertMa123@users.noreply.github.com>
2025-12-03 13:44:47 -0500

    additional imported csv's now have applied odd-row styling as well

--- public/js/edit_figure.js
+++ public/js/edit_figure.js
@@ -423,7 +423,12 @@ async function renderAll() {
 
             $('#checkboxes').append(`<h4>${name}</h4>`);
             for (const m of stats) {
-                $('#checkboxes').append(renderCheckBoxes(name, m));
+                const div = $(renderCheckBoxes(name, m));
+                div.addClass( idx%2 === 0? "even-row":"odd-row");
+                $('#checkboxes').append(div);
+
+                //console.log(idx);
+                idx++;
                 document.getElementById(`${name}_${m.monthYear}_Mean`)
                     .addEventListener('change', reloadFiltered);
                 document.getElementById(`${name}_${m.monthYear}_Median`)
